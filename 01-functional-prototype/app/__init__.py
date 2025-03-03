adbc0a2c18a07a3846dbd0548e04a27538db7b8e
John Baglio <30454621+absoluteAquarian@users.noreply.github.com>
2025-03-02 21:57:02 -0600

    Implement initial design + project fixes (#2)
    * Fixed project, created initial design for site
    
    * Change version requirements

--- 01-functional-prototype/app/__init__.py
+++ 01-functional-prototype/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from .config import Config
 from .models import db
 from .routes import main
 
 def create_app(config_filename='config.py'):
     app = Flask(__name__)
-    app.config.from_pyfile(config_filename)
+    app.config.from_object(Config)  # Default config
+    app.config.from_pyfile(config_filename)  # Load instance config
 
     db.init_app(app)
 
