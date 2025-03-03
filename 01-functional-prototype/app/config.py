ed229c777b5b6911953d54f6593eae7ed3682444
absoluteAquarian <jruss88@me.com>
2025-03-02 21:53:32 -0600

    Fixed project, created initial design for site

--- 01-functional-prototype/app/config.py
+++ 01-functional-prototype/app/config.py
@@ -2,4 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'default_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- 01-functional-prototype/app/routes.py
+++ 01-functional-prototype/app/routes.py
@@ -4,7 +4,15 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return render_template('index.html')
+    # Example list of (name, score) tuples
+    scores = [
+        ("Alice", 150),
+        ("Bob", 120),
+        ("Charlie", 100),
+        ("David", 90),
+        ("Eve", 80)
+    ]
+    return render_template('index.html', scores=scores)
 
 @main.route('/about')
 def about():
