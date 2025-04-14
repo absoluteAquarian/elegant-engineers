4cd0afd38fe30bffca7a749114d4470e5ef21512
absoluteAquarian <jruss88@me.com>
2025-04-13 23:28:30 -0500

    Fix missing module reference

--- 02-feature-expansion/tests/test_basic.py
+++ 02-feature-expansion/tests/test_basic.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
+from datetime import datetime
 from flask import url_for
 from app import create_app, db
 from app.models import Score, User
